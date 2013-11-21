4464f44bc546adf66288f2819c884b4b52d7bebd
Todd Yates <toddyates@ymail.com>
2013-11-21 01:10:52 +0000

    Basic sensor usage

--- Walkie/src/main/java/uk/co/bottleortree/walkie/MainActivity.java
+++ Walkie/src/main/java/uk/co/bottleortree/walkie/MainActivity.java
@@ -1,17 +1,25 @@
 package uk.co.bottleortree.walkie;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener{
+
+    private MainFragment mainFragment;
+
+    private SensorManager sensorManager;
+    private int stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
+            mainFragment = new MainFragment();
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, mainFragment)
                     .commit();
         }
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR), sensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,12 +63,34 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            stepCount ++;
+            refreshValues();
+        }
+    }
+
+    private void refreshValues() {
+        mainFragment.setStepCountDay(stepCount);
+        mainFragment.refreshValues();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not needed for step counters
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class MainFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        TextView stepCounterDay;
+
+        int stepCountDay = 0;
+
+        public MainFragment() {
         }
 
         @Override
@@ -59,6 +98,21 @@ public class MainActivity extends Activity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            stepCounterDay = (TextView)getView().findViewById(R.id.main_text_stepcounter_day);
+            refreshValues();
+        }
+
+        private void refreshValues() {
+            stepCounterDay.setText(Integer.toString(stepCountDay));
+        }
+
+        private void setStepCountDay(int count) {
+            stepCountDay = count;
         }
     }
 
